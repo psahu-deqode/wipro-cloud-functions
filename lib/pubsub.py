67029cf34025943d4d7be08b7994e7643e1328ba
Puja Sahu <psahu@deqode.com>
2021-03-23 19:08:12 +0530

    updated pubsub code so it won't create new topic every time

--- lib/pubsub.py
+++ lib/pubsub.py
@@ -1,14 +1,21 @@
+import google
 from google.cloud import pubsub_v1
-from google.oauth2 import service_account
+from google.auth.transport import requests
 
-from lib.datastore import PROJECT_ID
 
-credentials = service_account.Credentials
+credentials, project_id = google.auth.default()
+r = requests.Request()
+credentials.refresh(r)
+
 publisher = pubsub_v1.PublisherClient(credentials=credentials)
 
 
-def send_message(topic_name, launch_app_json):
-    topic_path = publisher.topic_path(PROJECT_ID, topic_name)
-    publisher.publish(topic_path, str(launch_app_json).encode("utf-8"))
+def send_message(topic_name, launch_app_json, vehicle_vin):
+    #topic_path = publisher.topic_path(PROJECT_ID, topic_name)
+    #topic_path = "projects/tripbot-cloud/topics/topic"
+    future = publisher.publish(topic_name, data=str(launch_app_json).encode("utf-8"), vin=vehicle_vin)
+    try:
+        print(future.result())
+    except Exception as e:
+        print("Error publishing: " + str(e))
     return
-
